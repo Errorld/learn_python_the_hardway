bde027d1804cb41a5989a187af618dd1a8c7ca13
Errorld <errorld@outlook.com>
2015-10-25 08:19:19 -0700

    pep8

--- ex45/classical.py
+++ ex45/classical.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 #!coding:utf-8
-from random import randint, uniform
 from sys import exit
+from random import randint, uniform
 import json
 
 
-def fighter2dict(std):
-    return {
-        'name':std.name,
-        'attack':std.attack,
-        'maxHp':std.maxHp,
-        'curHp':std.curHp,
-        'luck':std.luck,
-        'gold':std.gold
-    }
 class Fighter(object):
+
     def __init__(self, name):
         self.name = name
         self.atk = randint(1,10)
@@ -22,41 +14,50 @@ class Fighter(object):
         self.curHp = self.maxHp
         self.luck = 1
         self.gold = self.maxHp +self.atk
+
     def describe(self):
         return 'My name is %s.\nMy attack is %d.\nMy max hp is %d\nNow I have %d hp. I own %dgold' \
         % (self.name, self.atk, self.maxHp, self.curHp, self.gold)
+
     def attack(self, enemy):
         if not self.is_dead():
             value = (self.atk+(self.atk*(self.luck+uniform(-2,2)))*0.1)
             enemy.change_hp(-value)
             return "%s attacked %s, %dhp reduced. %dhp remains" % (self.name, enemy.name, value, enemy.curHp)
+
     def change_hp(self, value):
         self.curHp += value
         if self.curHp>self.maxHp:
             self.curHp = self.maxHp
         if self.is_dead():  #detect death
             return 'died'
+
     def loot(self, enemy):
         loot_gold = enemy.gold
         enemy.gold = 0
         self.gold += loot_gold
         return "%s robbed %d gold from %s's body" % (self.name, loot_gold, enemy.name)
+
     def is_dead(self):
         if self.curHp <= 0:
             return True
+
     def hack(self):
         self.atk = 10
         self.maxHp = 100
         print 'maxiumed your attr'
 
 class Scene(object):
+
     def enter(self, player):
         print 'not yet'
         return 'game_over'
 
 class Battle(Scene):
+
     def create_enemy(self):
         return Fighter('enemy')
+
     def battle(self, player, enemy):
         while True:
             print"""
@@ -104,6 +105,7 @@ class Battle(Scene):
         return 'main_menu'
 
 class Save(Scene):
+
     def enter(self, player):
         # print player
         # print json.dumps(player, default=lambda obj: obj.__dict__)
@@ -112,7 +114,9 @@ class Save(Scene):
             save.write(json.dumps(j),)
         print 'save finished in player.save'
         return 'main_menu'
+
 class Load(Scene):
+
     def enter(self, player):
         with open('player.save', 'r') as save:
             json_str = save.read()
@@ -132,6 +136,7 @@ class Load(Scene):
         return 'main_menu'
 
 class MainMenu(Scene):
+
     def show_menu(self,player):
         print 'Hello', player.name+':'
         print """
@@ -144,6 +149,7 @@ class MainMenu(Scene):
         6.Load
         0.Exit
         """
+
     def choose(self,player):
 
         choice = raw_input("> ")
@@ -169,33 +175,41 @@ class MainMenu(Scene):
             player.hack()
             return 'main_menu'
 
-    
     def enter(self, player):
         self.show_menu(player)
         return self.choose(player)
 
 class Status(Scene):
+
     def enter(self, player):
         print player.describe()
         raw_input()
         return 'main_menu'
     pass
+
 class Item(Scene):
     pass
+
 class Rest(Scene):
+
     def enter(self, player):
         player.curHp = player.maxHp
         print 'You had fully rested'
         raw_input()
         return 'main_menu'
+
 class GameOver(Scene):
+
     def enter(self, player):
         print 'Game Over'
         return 'exit'       
+
 class Exit(Scene):
+
     def enter(self, player):
         print 'See you'
         exit(0)
+
 class Control(object):
     scene = {
         'battle':Battle(),
@@ -208,20 +222,26 @@ class Control(object):
         'load':Load(),
         'exit':Exit()
     }
+
     def __init__(self, start_scene):
         self.openning_scene = start_scene
+
     def next_scene(self, scene_name):
         return Control.scene.get(scene_name)
+
     def start_scene(self):
         return self.next_scene(self.openning_scene)
 
 class Engine(object):
+
     def create_player(self):
         name = raw_input('tell me your name\n')
         return Fighter(name)
+
     def __init__(self, control):
         self.scene_control = control 
         self.player = self.create_player()
+
     def play(self):
         current_scene = self.scene_control.start_scene() 
         last_scene = self.scene_control.next_scene('exit')
